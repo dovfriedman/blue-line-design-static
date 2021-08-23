9488433bedaa27fd72e14885109c7f13836bf262
Dov Friedman <dov.friedman@wsj.com>
2021-08-23 19:35:50 -0400

    build

---
+++ _next/static/nDFv0m4AYtZmMBo5C-GQ1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b4a4e67691ad47a16e34.css","static/chunks/pages/index-7c2a04676629c6ce3edf.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
